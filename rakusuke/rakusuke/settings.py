fe2451571d6c97a5d3dec6600b69f650be87dc24
ketasaka <ykh2135032@stu.o-hara.ac.jp>
2022-10-31 16:12:57 +0900

    マイグレート

--- rakusuke/rakusuke/settings.py
+++ rakusuke/rakusuke/settings.py
@@ -1,4 +1,4 @@
-
+import os
 
 from pathlib import Path
 
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rakusukeapp.apps.RakusukeappConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
--- rakusuke/rakusukeapp/models.py
+++ rakusuke/rakusukeapp/models.py
@@ -1,9 +1,7 @@
-from accounts.models import CustomUser
 from django.db import models
 
 
 class rakusuke_schedule(models.Model):
-    user_id = models.ForeignKey(CustomUser,verbose_name='ユーザID', max_length=15, on_delete=models.PROTECT)
     schedule_do = models.TextField(verbose_name='やるべきこと', max_length=40)
     schedule_priority = models.IntegerField(verbose_name='優先度', max_length=1, default=2)
     schedule_start_date = models.DateTimeField(verbose_name='開始日時', auto_now=True)
