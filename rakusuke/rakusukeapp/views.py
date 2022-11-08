21f935add4092b9ab999632c74c266d854246e15
sato0125 <ykh.2135027@stu.o-hara.ac.jp>
2022-11-08 12:20:44 +0900

    スケジュール確認画面骨組み作成

--- rakusuke/rakusukeapp/views.py
+++ rakusuke/rakusukeapp/views.py
@@ -2,3 +2,9 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class OnedayschedulelistView(generic.TemplateView):
+    template_name = "onedayschedulelist.html"
+
+class OneweekschedulelistView(generic.TemplateView):
+    template_name = "oneweekschedulelist.html"
