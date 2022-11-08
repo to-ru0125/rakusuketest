56b0b45e21b63dfc0cdec5d1449b37af1b961b6c
ketasaka <ykh2135032@stu.o-hara.ac.jp>
2022-11-08 10:12:14 +0900

    models.py & calender.html

--- rakusuke/rakusukeapp/models.py
+++ rakusuke/rakusukeapp/models.py
@@ -6,12 +6,9 @@ class rakusuke_schedule(models.Model):
     schedule_priority = models.IntegerField(verbose_name='優先度', default=2)
     schedule_start_date = models.DateTimeField(verbose_name='開始日時', auto_now=True)
     schedule_end_date = models.DateTimeField(verbose_name='終了日時', auto_now=True)
-    schedule_start_hour = models.DateTimeField(verbose_name='開始日時(時)', default=0)
-    schedule_start_min = models.DateTimeField(verbose_name='開始日時(分)', default=0)
-    schedule_end_hour = models.DateTimeField(verbose_name='終了日時(時)', default=0)
-    schedule_end_min = models.DateTimeField(verbose_name='終了日時(分)', default=0)
     schedule_worktime = models.IntegerField(verbose_name='一日の作業時間')
     sucedule_creation_stage = models.IntegerField(verbose_name='作成段階')
+    sucedule_ditching = models.TextField(verbose_name='サボり日')
 
     class Meta:
         verbose_name_plural = 'rakusuke_schedule'
