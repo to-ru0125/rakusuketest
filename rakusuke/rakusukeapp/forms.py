146f704ee5544530763dff84997cabb9776e1ce8
katokota <ykh2135050@stu.o-hara.ac.jp>
2022-11-09 11:39:04 +0900

    加藤皓太2022/11/09/11:38

--- rakusuke/rakusukeapp/forms.py
+++ rakusuke/rakusukeapp/forms.py
@@ -4,13 +4,11 @@ from django import forms
 class ScheduleCreateForm(forms.ModelForm):
     class Meta:
         model = RakusukeSchedule
-        fields = ('schedule_start_date',
-                  'schedule_end_date',
-                  'schedule_do',
+        fields = ('schedule_do',
                   'schedule_category',
                   'schedule_worktime',
                   'schedule_priority',
-                  'sucedule_ditching')
+                  'schedule_ditching')
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             for field in self.fields.values():
