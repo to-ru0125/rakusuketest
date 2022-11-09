80f824d4a772df9ae0318d4394ec18b4afb7d913
ketasaka <ykh2135032@stu.o-hara.ac.jp>
2022-11-09 09:22:59 +0900

    11/8

--- rakusuke/rakusukeapp/views.py
+++ rakusuke/rakusukeapp/views.py
@@ -12,4 +12,19 @@ class OneweekschedulelistView(generic.TemplateView,LoginRequiredMixin):
     template_name = "oneweekschedulelist.html"
 
 class CalendarView(generic.TemplateView,LoginRequiredMixin):
+    # model = RakusukeSchedule
     template_name = "calendar.html"
+
+    # def get_queryset(self):
+    #     diaries = Shopping.objects.filter(user=self.request.user).order_by('-created_at')
+    #     return diaries
+    #
+    # def get_queryset(self):
+    #     q_word = self.request.GET.get('query')
+    #
+    #     if q_word:
+    #         diaries = Shopping.objects.filter((Q(product1__icontains=q_word) | Q(product2__icontains=q_word) | Q(product3__icontains=q_word) | Q(product4__icontains=q_word) | Q(product5__icontains=q_word) | Q(product6__icontains=q_word)), user = self.request.user).order_by('-created_at')
+    #         return diaries
+    #     else:
+    #         diaries = Shopping.objects.filter(user=self.request.user).order_by('-created_at')
+    #         return diaries
