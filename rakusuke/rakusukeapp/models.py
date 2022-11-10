f4f56313155f3e505801972ecd75fa54a1df305c
katokota <ykh2135050@stu.o-hara.ac.jp>
2022-11-10 15:45:11 +0900

    加藤皓太2022/11/10/15:45

--- rakusuke/rakusukeapp/models.py
+++ rakusuke/rakusukeapp/models.py
@@ -8,7 +8,7 @@ class RakusukeSchedule(models.Model):
     schedule_priority = models.IntegerField(verbose_name='優先度', default=2)
     schedule_start_date = models.DateTimeField(verbose_name='開始日時', auto_now=True)
     schedule_end_date = models.DateTimeField(verbose_name='終了日時', auto_now=True)
-    schedule_worktime = models.IntegerField(verbose_name='一日の作業時間')
+    schedule_worktime = models.IntegerField(verbose_name='１日の作業時間')
     schedule_creation_stage = models.IntegerField(verbose_name='作成段階')
     schedule_ditching = models.TextField(verbose_name='サボり日', blank=True, null=True)
     schedule_category = models.IntegerField(verbose_name='カテゴリ')
