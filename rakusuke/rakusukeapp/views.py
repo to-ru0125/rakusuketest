d6c6c8674b25aa40a04f8d876e786728a08c34ad
sato0125 <ykh.2135027@stu.o-hara.ac.jp>
2022-11-14 14:17:17 +0900

    ルーティングやったぜ

--- rakusuke/rakusukeapp/views.py
+++ rakusuke/rakusukeapp/views.py
@@ -4,23 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .forms import ScheduleCreateForm
 
-class CreateView(LoginRequiredMixin,generic.FormView):
-    model = RakusukeSchedule
-    template_name = 'make_schedule.html'
-    form_class = ScheduleCreateForm
-    success_url = reverse_lazy('成功後ページ')
-
-    def form_valid(self, form):
-        histories = form.save(commit=False)
-        histories.user = self.request.user
-        histories.save()
-        messages.success(self.request, '投稿しました。')
-        return super().form_valid(form)
-
-    def form_invalid(self, form):
-        messages.error(self.request, "投稿に失敗しました。")
-        return super().form_invalid(form)
-
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
@@ -34,3 +17,25 @@ class CalendarView(generic.TemplateView,LoginRequiredMixin):
     model = RakusukeSchedule
     template_name = "calendar.html"
 
+class FixedscheduleView(generic.TemplateView):
+    template_name = "fixedschedule.html"
+
+# class MakescheduleView(generic.TemplateView):
+#     template_name = 'makeschedule.html'
+
+class MakescheduleView(LoginRequiredMixin,generic.FormView):
+    model = RakusukeSchedule
+    template_name = 'makeschedule.html'
+    form_class = ScheduleCreateForm
+    success_url = reverse_lazy('成功後ページ')
+
+    def form_valid(self, form):
+        histories = form.save(commit=False)
+        histories.user = self.request.user
+        histories.save()
+        messages.success(self.request, '投稿しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "投稿に失敗しました。")
+        return super().form_invalid(form)
